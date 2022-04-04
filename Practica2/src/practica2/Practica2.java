d6b13b853e43e961ca61f6f4dd063beb85fdbfc2
jaume <jaume@DESKTOP-7VE5B1S>
2022-04-04 17:48:25 +0200

    Canvi1

--- Practica2/src/practica2/Practica2.java
+++ Practica2/src/practica2/Practica2.java
@@ -1,21 +1,9 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package practica2;
 
 import java.util.Scanner;
 
-/**
- *
- * @author esther
- */
 public class Practica2 {
 
-    /**
-     * @param args the command line arguments
-     */
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         double res = 0;
