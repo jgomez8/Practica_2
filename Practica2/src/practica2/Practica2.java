b526907c755116b77947d2b4c4c6d1ea47815999
jaume <jaume@DESKTOP-7VE5B1S>
2022-04-07 17:54:50 +0200

    Metode2

--- Practica2/src/practica2/Practica2.java
+++ Practica2/src/practica2/Practica2.java
@@ -1,34 +1,16 @@
 package practica2;
-
 import static practica2.Metodes.*;
 
 public class Practica2 {
-
     public static void main(String[] args) {
+        
 
         do {
 
             menuOperacions();
 
-            System.out.println("(" + numero1 + ") " + operacion + " (" + numero2 + ")" + " = " + res);
-            System.out.println("\n Vols continuar operant? \n");
-            System.out.println(" [s/n]");
-            do {
-                comprobar = true;
-                operacion = sc.nextLine();
-
-                switch (operacion) {
-                    case "s":
-                    case "S":
-                    case "n":
-                    case "N":
-                        break;
-                    default:
-                        System.err.println("\n Error, posa un valor vàlid. \n");
-                        comprobar = false;
-                }
-            } while (comprobar != true);
-
+            sortida();
+            
         } while (operacion.equalsIgnoreCase("s"));
     }
 }
